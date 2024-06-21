59d2c18f77dff3ac91d120f5ea4a320fe8844e0e
Caleb Rowland <cjrowland4@gmail.com>
2024-06-21 08:19:06 -0500

    Add final stage

--- main.js
+++ main.js
@@ -4,10 +4,40 @@ const wordChoices = ["python", "java", "swift", "javascript"];
 
 
 function main() {
-  console.log("H A N G M A N\n")
-  let gameWord = getRandomElement(wordChoices);
-  gameLoop(gameWord);
-  console.log("Thanks for playing!");
+  console.log("H A N G M A N");
+  let results = [];
+
+  while (true) {
+    let choice = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: ');
+
+    switch (choice) {
+      case "play":
+        results.push(gameLoop());
+        break;
+      case "results":
+        displayResults(results);
+        break;
+      case "exit":
+        console.log("Thanks for playing!");
+        return;
+    }
+  }
+}
+
+function displayResults(results) {
+  let wins = 0;
+  let losses = 0;
+
+  for (const result of results) {
+    if (result === "win") {
+      wins++
+    } else if (result === "loss") {
+      losses++
+    }
+  }
+
+  console.log(`You won: ${wins} times.`);
+  console.log(`You lost: ${losses} times`);
 }
 
 
@@ -17,7 +47,8 @@ function getRandomElement(elements) {
 }
 
 
-function gameLoop(gameWord) {
+function gameLoop() {
+  let gameWord = getRandomElement(wordChoices);
   let remainingGuesses = 8;
   let wordProgress = "-".repeat(gameWord.length);
   let previousGuesses = [];
@@ -27,7 +58,7 @@ function gameLoop(gameWord) {
     if (!wordProgress.includes("-")) {
       console.log(`You guessed the word ${wordProgress}!`);
       console.log("You survived!");
-      return
+      return "win"
     }
 
     let guess = input("Input a letter: ");
@@ -47,6 +78,7 @@ function gameLoop(gameWord) {
     }
   }
   console.log("You lost!");
+  return "loss"
 }
 
 
